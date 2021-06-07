a7f2924ab94d5d3d9507f1004a3ed9f00b6d059a
Horacio Duca <horacio.duca@globant.com>
2021-06-07 13:45:59 -0300

    Uploading my Business Credential Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,185 @@
 import 'package:flutter/material.dart';
+import 'package:business_card/styles/dimensions.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(BusinessCardApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class BusinessCardApp extends StatelessWidget {
+  BusinessCardApp({
+    this.blurRadius = Dimension.blurRadius,
+    this.spreadRadius = Dimension.spreadRadius,
+    this.circleAvatarRadius = Dimension.circleAvatarRadius,
+    this.titleFontSize = Dimension.titleFontSize,
+    this.subtitleFontSize = Dimension.subtitleFontSize,
+    this.letterSpacing = Dimension.letterSpacing,
+    this.dividerHeight = Dimension.dividerHeight,
+    this.cardSubtitleFontSize = Dimension.cardSubtitleFontSize,
+  });
+
+  final double blurRadius;
+  final double spreadRadius;
+  final double circleAvatarRadius;
+  final double titleFontSize;
+  final double subtitleFontSize;
+  final double letterSpacing;
+  final double dividerHeight;
+  final double cardSubtitleFontSize;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      debugShowCheckedModeBanner: false,
+      title: 'Business Card App',
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.green,
+              Colors.yellow,
+            ]),
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 50.0,
+                right: 50.0,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: blurRadius,
+                            color: Colors.black,
+                            spreadRadius: spreadRadius,
+                          ),
+                        ]),
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage("assets/images/me.jpg"),
+                      radius: circleAvatarRadius,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  Text(
+                    'Horacio Duca',
+                    style: TextStyle(
+                      fontSize: titleFontSize,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      shadows: [
+                        Shadow(
+                          blurRadius: blurRadius,
+                          color: Colors.black,
+                          offset: Offset(
+                            0.0,
+                            -2.0,
+                          ),
+                        ),
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.lightGreen,
+                          offset: Offset(
+                            5.0,
+                            -5.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    'Flutter Developer Trainee',
+                    style: TextStyle(
+                      fontSize: subtitleFontSize,
+                      letterSpacing: letterSpacing,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      shadows: [
+                        Shadow(
+                          blurRadius: blurRadius,
+                          color: Colors.black,
+                          offset: Offset(
+                            0.0,
+                            -2.0,
+                          ),
+                        ),
+                        Shadow(
+                          blurRadius: blurRadius,
+                          color: Colors.lightGreen,
+                          offset: Offset(
+                            5.0,
+                            -5.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: 220.0,
+                    height: 10.0,
+                    child: Divider(
+                      height: dividerHeight,
+                      color: Colors.black54,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.lightGreen,
+                    child: ListTile(
+                      leading: Icon(Icons.phone),
+                      title: Text(
+                        '+54 9 2284-534691',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        'Press and call me!',
+                        style: TextStyle(
+                          fontSize: cardSubtitleFontSize,
+                        ),
+                      ),
+                      onTap: () => _onPressMakePhoneCall(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Card(
+                    color: Colors.lightGreen,
+                    child: ListTile(
+                      leading: Icon(Icons.email_outlined),
+                      title: Text(
+                        'Horacio.duca@globant.com',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        'Or send me an email',
+                        style: TextStyle(
+                          fontSize: cardSubtitleFontSize,
+                        ),
+                      ),
+                      onTap: () => _onPressSendEmail(),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  _onPressSendEmail() {}
+
+  _onPressMakePhoneCall() {}
 }
---
+++ lib/styles/dimensions.dart
@@ -0,0 +1,10 @@
+abstract class Dimension{
+  static const double blurRadius = 10.0;
+  static const double spreadRadius = 5.0;
+  static const double circleAvatarRadius = 100.0;
+  static const double titleFontSize = 32.0;
+  static const double subtitleFontSize = 18.0;
+  static const double letterSpacing = 2.0;
+  static const double dividerHeight = 2.0;
+  static const double cardSubtitleFontSize = 13.0;
+}
